94a490bbfed5d85548a15b0f8a9e66feb2845286
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-08-27 14:16:21 -0400

    Update config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "/zarr-summit-2025",
+  assetPrefix: "/zarr-summit-2025",
   images: {
     unoptimized: true,
   },
