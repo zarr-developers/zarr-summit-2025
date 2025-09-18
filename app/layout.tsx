bec600bce7680cefd62ec84ad93ef35a3eecf30c
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-09-18 13:46:18 -0400

    Add sponsor page

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,15 @@ export default function RootLayout({
                 </li>
                 <li>
                   <a
+                    href="/zarr-summit-2025/zarr-summit-prospectus.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Sponsor
+                  </a>
+                </li>
+                <li>
+                  <a
                     href="/zarr-summit-2025/register"
                     className="register-link"
                   >
